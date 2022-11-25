fef6e5cf0e5c65c7d3e8b2aaabd50b598ccc5383
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2022-11-25 14:58:50 -0300

    Add graph tutorial

---
+++ Catlike Coding Tutorials/Basics/Graph/Editor/GraphEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Graph))]
+public class EditorGraph : Editor{
+    public override void OnInspectorGUI(){
+        DrawDefaultInspector();
+        var graphScript = (Graph) target;
+        if (GUILayout.Button("Build Graph")) graphScript.BuildGraph();
+    }
+}
--- Catlike Coding Tutorials/Basics/Graph/Graph.cs
+++ Catlike Coding Tutorials/Basics/Graph/Graph.cs
@@ -1,24 +1,55 @@
 using UnityEngine;
 
-public class Graph : MonoBehaviour {
+public class Graph : MonoBehaviour{
     [SerializeField] private Transform cube;
     [SerializeField, Range(10, 100)] private int resolution = 10;
 
-    private void Awake() {
+    private int lastResolution;
+    private Transform[] points;
+
+    private void Awake(){
         BuildGraph();
     }
 
-    private void BuildGraph() {
+    private void Update(){
+        UpdateGraph();
+    }
+
+    private void OnValidate(){
+        if (Application.isPlaying && lastResolution != resolution){
+            lastResolution = resolution;
+            BuildGraph();
+        }
+    }
+
+    private static float Function(float x){
+        //return Mathf.Pow(((x - 1) % 2) - 1, 3);
+        return Mathf.Sin(x * Mathf.PI);
+    }
+
+    private void UpdateGraph(){
+        Vector3 position = Vector3.zero;
+        float time = Time.time;
+        foreach (Transform point in points){
+            position.x = point.localPosition.x;
+            position.y = Function(position.x + time);
+            point.localPosition = position;
+        }
+    }
+
+    public void BuildGraph(){
+        points = new Transform[resolution];
+        for (int i = 0; i < transform.childCount; i++) Destroy(transform.GetChild(i).gameObject);
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++) {
-            Transform point = Instantiate(cube);
+        for (int i = 0; i < resolution; i++){
+            Transform point = Instantiate(cube, transform, false);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
+            position.y = Function(position.x);
             point.localPosition = position;
             point.localScale = scale;
-            point.SetParent(transform, false);
+            points[i] = point;
         }
     }
 }
